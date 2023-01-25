b3e007a853507bcbf881ca527f883cc0536a2ad8
Comondear <elcomondear@gmail.com>
2023-01-25 13:59:33 -0700

    fix-buttons

--- src/layout/common/Navigation/Navigation.jsx
+++ src/layout/common/Navigation/Navigation.jsx
@@ -1,49 +1,47 @@
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import authSelectors  from "redux/auth/authSelectors";
-import styled from "styled-components"
-import { breakpoints } from "styleConfig/breakpoints";
-
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/authSelectors';
+import styled from 'styled-components';
+import { breakpoints } from 'styleConfig/breakpoints';
 
 export const Navigation = () => {
-const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   return (
     <Nav>
-      <Link to='/'>Home</Link>
-        {isLoggedIn && 
-      <Link to='/contacts'>Contacts</Link>}
+      <Link to="/">Home</Link>
+      {isLoggedIn && <Link to="/contacts">Contacts</Link>}
     </Nav>
   );
 };
 
 const Nav = styled.nav`
-display: flex;
-align-items: center;
-justify-content: flex-start;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `;
 const Link = styled(NavLink)`
-transition: all .3s ease;
-padding: ${p => p.theme.space[2]}px 0;
-color: ${p => p.theme.colors.primary};
-font-family: ${p => p.theme.fonts.body};
-font-size: ${p => p.theme.fontSizes.l};
-font-weight: ${p => p.theme.fontWeights.bold};
-  &:not(:last-child){
+  transition: all 0.3s ease;
+  padding: ${p => p.theme.space[2]}px 0;
+  color: ${p => p.theme.colors.primary};
+  font-family: ${p => p.theme.fonts.body};
+  font-size: ${p => p.theme.fontSizes.l};
+  font-weight: ${p => p.theme.fontWeights.bold};
+  &:not(:last-child) {
     margin-right: ${p => p.theme.space[2]}px;
-  };
+  }
   @media (${breakpoints.tablet}) {
     font-size: ${p => p.theme.fontSizes.xl};
     padding: ${p => p.theme.space[3]}px 0;
-    &:not(:last-child){
+    &:not(:last-child) {
       margin-right: ${p => p.theme.space[2]}px;
-    };
-  };
+    }
+  }
   &.active {
-    color: ${p => p.theme.colors.accentLight}
-  };
+    color: ${p => p.theme.colors.accent};
+  }
   &:hover {
-    color: ${p => p.theme.colors.accentLight};
+    color: ${p => p.theme.colors.accent};
     cursor: pointer;
-  };
+  }
 `;
